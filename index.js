69be53cbd80837ca12e72227e0e39adb438b6f92
Swung0x48 <swung0x48@outlook.com>
2020-08-12 21:13:43 +0800

    Handling rejections and cleanup.

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const puppeteer = require('puppeteer');
 // const http = require('http');
 const readline = require('readline');
 const fs = require('fs');
-const pathPrefix = 'content/'
+const pathPrefix = 'content/';
+const screenshotPathPrefix = 'screenshot/';
 
 const rl = readline.createInterface({
     input: process.stdin,
@@ -56,91 +57,82 @@ async function log(level, detail) {
     console.log('[' + level + '] ' + detail);
 }
 
+async function onReject(reason, detail, page) {
+    log('ERR', detail);
+    log('ERR', 'Reason: ' + reason);
+    await page.screenshot({path: pathPrefix + 'onError.png'});
+
+    process.exit(1);
+}
+
 async function emuBrowser(cred) {
     await log('INFO', 'Starting puppeteer...');
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
     // await page.setViewport({ width: 750, height: 1334 });
 
-    // await log('INFO', 'Fetching cookie from file...');
-    // ProcessCookie().then(value => {
-    //     log('INFO', 'Set cookie from file...');
-    //     page.setCookie(...value);
-    // }, reason => {
-    //     log('WARN', reason);
-    // });
     await page.goto('https://www.shou.edu.cn/');
     await page.screenshot({
         path: pathPrefix + 'init.png'
     });
 
-    // await log('INFO', 'Dry run: Test if cookies has expired...');
-    // let url = await page.url();
-    // if (url === "https://workflow.shou.edu.cn/taskcenter/workflow/index")
-    // {
-    //     await log('INFO', 'Cookie OK.');
-    // }
     await log('INFO', 'Logging in...');
 
     /* --- Login --- */
     await page.type('input[name="username"]', cred.username);
     await page.type('input[name="password"]', cred.password);
+    await page.screenshot({path: pathPrefix + 'login.png'});
     await page.click('input[name="submit"]');
 
-    await page.screenshot({path: pathPrefix + 'login.png'});
-
-    await page.waitForNavigation({
-        waitUntil: 'networkidle0',
+    page.waitForSelector(".c_4f9dff").then(value => {
+        log('INFO', 'Login succeeded.');
+    }, reason => {
+        onReject(reason, 'Login failed', page);
     });
     await page.screenshot({
-        path: pathPrefix + 'post-login.png'
+        path: pathPrefix + 'post_login.png'
     });
 
-    // const element = await page.$("img");
-    // const captcha = await element.screenshot({
-    //     path: pathPrefix + "captcha.png"
-    // });
-    // http.createServer(function (req, res) {
-    //     res.writeHead(200, {'Content-Type': 'image/jpeg'})
-    //     res.end(captcha) // Send the file data to the browser.
-    // }).listen(8124)
-    // console.log('Go to http://localhost:8124/ for captcha.');
-    // let captchaVal = await captchaInput();
-    // await page.type('#imageCodeName', captchaVal);
-    // await page.click("input[value='提交']");
-
-
-    /* --- Login Complete --- */
-    //
-    // /* --- Save Cookie --- */
-    // let cookies = await page.cookies()
-    // console.log(cookies);
-    // fs.writeFileSync(pathPrefix + 'cookie.json', JSON.stringify(cookies));
-    // /* --- Save Cookie Complete --- */
-
-
     await page.goto('https://workflow.shou.edu.cn/infoplus/form/XSJKSBLJ/start');
+    await log('INFO', 'Redirecting...');
+
+    await page.screenshot({path: pathPrefix + 'pre-report.png'});
+
+    await log('INFO', 'Waiting for report UI to show up...');
+    page.waitForSelector('#V1_CTRL82').then(value => {
+        log('INFO', 'Report UI OK.');
+    }, reason => {
+        onReject(reason, 'Report UI does not show up.', page);
+    });
+    // await page.waitForSelector("#V1_CTRL82"); // TODO
+    await page.click("#V1_CTRL82");
+
+    await page.waitForSelector(".command_button_content");
+    await page.click(".command_button_content");
+
+    await page.waitForSelector(".dialog_button.default.fr");
+    await page.click(".dialog_button.default.fr");
+    // await page.waitForSelector(".dialog_button.default.fr");
     await page.waitForNavigation({
         waitUntil: 'networkidle0',
     });
-
-    // let tmp = await page.url();
-    // console.log(tmp);
-    await page.screenshot({path: pathPrefix + 'pre-report.png'});
-
-    cookies = await page.cookies()
-    console.log(cookies);
-
-    // await page.click("#V1_CTRL82");
-    // await page.click("#infoplus_action_4789_1");
+    await log('INFO', 'Report succeed.');
+    let screenshotPath = 'report-' + new Date().toISOString() + '.png'
+    await page.screenshot({path: pathPrefix + screenshotPathPrefix + screenshotPath});
+    await log('INFO', 'Screenshot saved at: ' + pathPrefix + screenshotPathPrefix + screenshotPath);
 
     await browser.close();
+    await log('INFO', 'Closing browser...');
+    await log('INFO', 'Exiting...');
     process.exit(0);
 }
 
 (async () => {
     if (!fs.existsSync(pathPrefix)) {
         fs.mkdirSync(pathPrefix);
+    }
+    if (!fs.existsSync(pathPrefix + screenshotPathPrefix)) {
+        fs.mkdirSync(pathPrefix + screenshotPathPrefix);
     }
     const cred = await credInput();
     await emuBrowser(cred);
